dc91c49a9b661881d46546220fe8cfc5f8e41ae9
ratnikova <barbienot@mail.ru>
2018-10-24 14:21:21 +0300

    lesson 2

--- src/by/it/ratnikova/lesson02/TaskB3.java
+++ src/by/it/ratnikova/lesson02/TaskB3.java
@@ -11,11 +11,11 @@ package by.it.ratnikova.lesson02;
 class TaskB3 {
         public static void main(String[] args) {
 //        String s = "C";
-//        System.out.println("Новым Годом");
+//        System.out.println();
 //        System.out.println("C Новым");
 //        System.out.println("Годом");
 //        System.out.print("C Новым");
-//        System.out.println("Годом");
+//        System.out.print("Годом");
 //        System.out.println(s);
 //        System.out.print("C Новым!");
 //        System.out.print(" ");
