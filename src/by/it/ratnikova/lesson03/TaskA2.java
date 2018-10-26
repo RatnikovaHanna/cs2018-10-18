5790fda69af37e510d44654990aa1d8c380b5288
ratnikova <barbienot@mail.ru>
2018-10-26 19:33:18 +0300

    lesson 3

--- src/by/it/ratnikova/lesson03/TaskA2.java
+++ src/by/it/ratnikova/lesson03/TaskA2.java
@@ -1,4 +1,4 @@
-package by.it._tasks_.lesson03;
+package by.it.ratnikova.lesson03;
 
 /*
 Lesson 03. Task A2. Стихотворение
@@ -21,5 +21,15 @@ Lesson 03. Task A2. Стихотворение
 Для автоматической проверки откройте и запустите класс Testing
 */
 class TaskA2 {
+
+    public static void main(String[] args) {
+        String a ="Мороз и солнце; день чудесный!" ;
+        String b ="Еще ты дремлешь, друг прелестный" ;
+        String c = "Пора, красавица, проснись";
+        String d = "Открой сомкнуты негой взоры";
+        System.out.println("Мое любимое стихотворение:");
+        System.out.println(a+"\n"+b + "\n" + c+ "\n" + d );
+
+    }
 
 }
